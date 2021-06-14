86f89ee5405ffb9e1bbc2c34fbc915f463279165
Liam Walsh <lwalsh_1980@hotmail.co.uk>
2021-06-14 19:19:19 +0000

    new JS file added to JS folder for contact form

---
+++ assets/js/sendEmail.js
@@ -0,0 +1,15 @@
+function sendMail (contactForm) {
+    emailjs.send("service_vg5bikj","the_euro_football_quiz",{
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.emailaddress.value,
+        "feedback": contactForm.feedback.value
+    })
+    .then(
+        function(response) {
+            console.log("SUCCESS", response);
+        },
+        function (error) {
+            console.log("FAILED", error);
+        });
+        return false;
+}
