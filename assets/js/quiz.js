5ad4c934b2662bb457c82949231adf7ebca80acd
Liam Walsh <lwalsh_1980@hotmail.co.uk>
2021-06-16 12:31:23 +0000

    further Javascript added to quiz.html page

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,32 +1,43 @@
+// Sets up all users score to be zero before starting the quiz questions
 let finalScore = 0;
 
+// Function will only run when the page is fully loaded
 $(document).ready(function(){
+
+//  Highlights the answer with a grey background color if incorrect and an orange background color if correct
     $(".quiz-btn").click(function() {
         if ($(this).hasClass('true')) {
             $(this).addClass('quiz-btn-correct');
         } else {
-            $(this).addClass('quiz-btn-correct');
+            $(this).addClass('quiz-btn-incorrect');
         }
     });
 
-    $('.true').one('click', function() {
+    
+
+// Increments the users score by 1 each time they get a question correct 
+    $('.true').click(function() {
         finalScore ++;
     });
 
+// 
     $("#submit-answers-btn").click(function() {
-        $("#submit-answers-btn").text('Your score is: ' + finalScore + ' out of 6');
+        $('.hide-btn').show();
+        $(".results-btn").text(+ finalScore + ' out of 6');
+        $(".answers-btn");
+        $(".try-again-btn");
+        
     });
 
-
-    // $('.quiz-btn').click(function() {
-    //     $(this).siblings.remove();
-    // })
-
-
     
-    
+    $("#submit-answers-btn").click(function() {
+        $("#submit-answers-btn").remove();
+    });
 })
 
+// $(".quiz-btn").click(function() {
+    //     $(this).siblings.remove();
+    // })
 
 // $("#submit-answers-btn").click(function() {
 //     $("#submit-answers-btn").text("hello");
