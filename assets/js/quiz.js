1fd346b6af50864385890d4693d6f8c0b5e93ecc
Liam Walsh <lwalsh_1980@hotmail.co.uk>
2021-06-18 14:20:17 +0000

    JS code added for relevant answer buttons to be removed

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -2,10 +2,10 @@
 let finalScore = 0;
 
 // Function will only run when the page is fully loaded
-$(document).ready(function(){
+$(document).ready(function () {
 
-//  Highlights the answer with a grey background color if incorrect and an orange background color if correct
-    $(".quiz-btn").click(function() {
+    //  Highlights the answer with a grey background color if incorrect and an orange background color if correct
+    $(".quiz-btn").click(function () {
         if ($(this).hasClass('true')) {
             $(this).addClass('quiz-btn-correct');
         } else {
@@ -13,49 +13,121 @@ $(document).ready(function(){
         }
     });
 
-// create a function that either removes the other two answer once an answer has been selected, or locks in the answer once clicked
-    
-
-// Increments the users score by 1 each time they get a question correct 
-    $('.true').click(function() {
-        finalScore ++;
+    // Increments the users score by 1 each time they get a question correct 
+    $('.true').click(function () {
+        finalScore++;
     });
 
-// Show users score by removing the submit answer button and showing the users score and options to look at the answer or try again
-    $("#submit-answers-btn").click(function() {
+    // Show users score by removing the submit answer button and showing the users score and options to look at the answer or try again
+    $("#submit-answers-btn").click(function () {
         $('.hide-btn').show();
         $(".results-btn").text(finalScore + ' out of 6');
         $(".answers-btn");
         $(".try-again-btn");
     });
 
-    $("#submit-answers-btn").click(function() {
+    $("#submit-answers-btn").click(function () {
         $("#submit-answers-btn").remove();
     });
 
-    $(".quiz-btn").click(function() {
-        $(this).siblings.remove();
+    // question 1 - removes 2 answers out of 3 when 1 answer is selected
+    $("#q1a1").click(function () {
+        $("#q1a2").remove();
+        $("#q1a3").remove();
     });
 
-    
+    $("#q1a2").click(function () {
+        $("#q1a1").remove();
+        $("#q1a3").remove();
+    });
+
+    $("#q1a3").click(function () {
+        $("#q1a1").remove();
+        $("#q1a2").remove();
+    });
+
+    // question 2 - removes 2 answers out of 3 when 1 answer is selected
+    $("#q2a1").click(function () {
+        $("#q2a2").remove();
+        $("#q2a3").remove();
+    });
+
+    $("#q2a2").click(function () {
+        $("#q2a1").remove();
+        $("#q2a3").remove();
+    });
+
+    $("#q2a3").click(function () {
+        $("#q2a1").remove();
+        $("#q2a2").remove();
+    });
+
+    // question 3 - removes 2 answers out of 3 when 1 answer is selected
+    $("#q3a1").click(function () {
+        $("#q3a2").remove();
+        $("#q3a3").remove();
+    });
+
+    $("#q3a2").click(function () {
+        $("#q3a1").remove();
+        $("#q3a3").remove();
+    });
+
+    $("#q3a3").click(function () {
+        $("#q3a1").remove();
+        $("#q3a2").remove();
+    });
+
+    // question 4 - removes 2 answers out of 3 when 1 answer is selected
+    $("#q4a1").click(function () {
+        $("#q4a2").remove();
+        $("#q4a3").remove();
+    });
+
+    $("#q4a2").click(function () {
+        $("#q4a1").remove();
+        $("#q4a3").remove();
+    });
+
+    $("#q4a3").click(function () {
+        $("#q4a1").remove();
+        $("#q4a2").remove();
+    });
+
+    // question 5 - removes 2 answers out of 3 when 1 answer is selected
+    $("#q5a1").click(function () {
+        $("#q5a2").remove();
+        $("#q5a3").remove();
+    });
+
+    $("#q5a2").click(function () {
+        $("#q5a1").remove();
+        $("#q5a3").remove();
+    });
+
+    $("#q5a3").click(function () {
+        $("#q5a1").remove();
+        $("#q5a2").remove();
+    });
+
+    // question 6 - removes 2 answers out of 3 when 1 answer is selected
+    $("#q6a1").click(function () {
+        $("#q6a2").remove();
+        $("#q6a3").remove();
+    });
+
+    $("#q6a2").click(function () {
+        $("#q6a1").remove();
+        $("#q6a3").remove();
+    });
+
+    $("#q6a3").click(function () {
+        $("#q6a1").remove();
+        $("#q6a2").remove();
+    });
 })
 
-// $(".quiz-btn").click(function() {
-//         $(this).siblings.remove();
-//     })
-
-// $("#submit-answers-btn").click(function() {
-//     $("#submit-answers-btn").text("hello");
-// });
-
-// $("#submit-answers-btn").click(function() {
-//     $("#submit-answers-btn").remove();
-// });
-
-
-// $("#submit-answers-btn").click(function() {
-//     $("#submit-answers-btn").remove();
-//     $("#submit-answers-btn").add();
-// });
-
+    // $(".quiz-btn").click(function() {
+    //     $(this).siblings.remove();
+    // });
 
