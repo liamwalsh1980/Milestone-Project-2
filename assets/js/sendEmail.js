82aa371fd1bb251533bc2a864ec2af617c825965
Liam Walsh <lwalsh_1980@hotmail.co.uk>
2021-06-18 14:20:44 +0000

    JS code added to support modal working

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,22 +1,37 @@
-function sendMail (contactForm) {
-    emailjs.send("service_vg5bikj","the_euro_football_quiz",{
-        "from_name": contactForm.name.value,
-        "from_email": contactForm.emailaddress.value,
-        "feedback": contactForm.feedback.value
-    })
-    .then(
-        function(response) {
-            console.log("SUCCESS", response);
-            $(".modal-message").text("Thank you for your feedback " + contactForm.name.value +". We hope you enjoyed the quiz");
-            $("#closing-btn").click(function() {
-                location.reload();
+function sendMail(contactForm) {
+    var modal = document.getElementById("modal");
+
+    emailjs.send("service_vg5bikj", "the_euro_football_quiz", {
+            "from_name": contactForm.name.value,
+            "from_email": contactForm.emailaddress.value,
+            "feedback": contactForm.feedback.value
+        })
+        .then(
+            function (response) {
+                console.log("SUCCESS", response);
+                // Bespoke message to the user specifically to the 'name' of the user within the Modal
+                $(".modal-message").text("Thank you for your feedback " + contactForm.name.value + ". We hope you enjoyed the quiz");
+                modal.style.display = "block"
+                // reloads the form once the user clicks the close button in the Modal
+                $("#closing-btn").click(function () {
+                    location.reload();
+                });
+            },
+            function (error) {
+                console.log("FAILED", error);
+                // Bespoke message to the user specifically to the 'name' of the user within the Modal
+                $(".modal-message").text("Sorry " + contactForm.name.value + " something went wrong. Please try submitting your feedback again!");
+                modal.style.display = "block"
+                // reloads the form once the user clicks the close button in the Modal
+                $("#closing-btn").click(function () {
+                    location.reload();
+                });
             });
-        },
-        function (error) {
-            console.log("FAILED", error);
-        });
-        return false;
+
+
+    return false;
 }
 
 // Source of code from EmailJS using Email templates
 // Testing completed as credit limited reached for monthly - see README for more information
+// Used to source the code for reloading the form - https://stackoverflow.com/questions/5404839/how-can-i-refresh-a-page-with-jquery
